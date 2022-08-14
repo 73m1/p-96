c305cb1c715e785ce56b972a057d38d7e6122068
73m1 <87555478+73m1@users.noreply.github.com>
2022-08-14 18:16:46 +1000

    Add files via upload

---
+++ kwitter_page.js
@@ -0,0 +1,16 @@
+
+//ADD YOUR FIREBASE LINKS HERE
+var firebaseConfig = {
+    apiKey: "AIzaSyCuh3zIz5QS0rYjJN1s7K7VfkTR4Nt-xZI",
+    authDomain: "database-1-caa2b.firebaseapp.com",
+    databaseURL: "https://database-1-caa2b-default-rtdb.firebaseio.com",
+    projectId: "database-1-caa2b",
+    storageBucket: "database-1-caa2b.appspot.com",
+    messagingSenderId: "426065953320",
+    appId: "1:426065953320:web:20ab96a6cd922d252f2b1d",
+    measurementId: "G-D4814H5920"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  
